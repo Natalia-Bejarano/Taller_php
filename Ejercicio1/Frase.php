8dfe8a6040eb91b3f6b5f55718208c01cf30db29
Sala D2 <CSalaD2@uniboyaca.edu.co>
2025-03-22 11:38:40 -0500

    se crean carpetas y se asignan partes

---
+++ Ejercicio1/Frase.php
@@ -0,0 +1,51 @@
+<?php
+    class Frase{
+        public $palabra;
+        public $separador;
+    }
+    function separador(){
+        
+    }
+    function set_palabra($palabra){
+        $this->palabra = $palabra;
+    }
+    function get_palabra($palabra){
+        return $this->palabra;
+    }
+    function set_separador($separador){
+        $this->separador = $separador;
+    }
+    function get_separador($separador){
+        return $this->separador;
+    }
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
